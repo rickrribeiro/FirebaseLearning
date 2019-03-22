192c4ffa3542d7c49df8b95ee41c70d233fb512c
ricardobah <rickribeiro99@gmail.com>
2019-03-22 16:08:30 -0300

    firebase node

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,66 @@
-const functions = require('firebase-functions');
-const Telegraf = require('telegraf')
-const apixu = require('apixu')
-const apixuClient = new apixu.Apixu({
-    apikey: '884b09dfacef4179952192451192103'
-});
-const bot = new Telegraf('TOKEN AQ') //TODO
-bot.start((ctx) => ctx.reply('Welcome. Do you want to view google or twitter trends?'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+    const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    apixuClient.current('salvador').then((current) =>{  
-         return response.send(current)
-        }).catch((err) =>{
-           return response.send(err)
-        })
+    const firebaseAdmin = require('firebase-admin')
+
+
+    config={
+        apikey:  '884b09dfacef4179952192451192103',
+        projectId: "trendsbot-a91a6",
+        authDomain: "trendsbot-a91a6",
+        databaseURL: "https://trendsbot-a91a6.firebaseio.com",
+        storageBucket: "trendsbot-a91a6.appspot.com"
+    };
+    firebaseAdmin.initializeApp(config)
+    var db = firebaseAdmin.firestore()
+
+
+    var docRef = db.collection('users').doc('alovelace');
+
+    var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+    });
+
+    var aTuringRef = db.collection('cities').doc('1wjaz7w1KSvPv0CYo432');
+
+    var setAlan = aTuringRef.set({
+        'first': 'Alan2',
+        'middle': 'Mathison',
+        'last': 'Turing',
+        'born': 1912
+    }, {merge: true});
     
-});
 
+    var addDoc = db.collection('cities').add({
+        name: 'Tokyo',
+        country: 'Japan'
+      }).then(ref => {
+        console.log('Added document with ID: ', ref.id);
+      });
+
+      var deleteDoc = db.collection('cities').doc('1nsYMJccpyxqKfXfzC7R').delete()
+
+      var cityRef = db.collection('cities').doc('1wjaz7w1KSvPv0CYo432');
+      var getDoc = cityRef.get()
+        .then(doc => {
+          if (!doc.exists) {
+            console.log('No such document!');
+          } else {
+            console.log('Document data:', doc.data());
+          }
+        })
+        .catch(err => {
+          console.log('Error getting document', err);
+        });
+
+        var allCities = db.collection('cities').get() //getall
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+          });
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+        });
+      
