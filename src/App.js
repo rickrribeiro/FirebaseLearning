237d424cddd319dd2c55c9e899fbb48e4488438b
ricardobah <rickribeiro99@gmail.com>
2019-03-26 13:48:18 -0300

    project details component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar.jsx'
 import Dashboard from './components/dashboard/Dashboard.jsx'
+import ProjectDetails from './components/projects/ProjectDetails.js'
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,8 @@ class App extends Component {
          <Navbar />
          <Switch>
            <Route path='/' component={Dashboard} />
+           <Route path = '/project/:id' component = {ProjectDetails} />
+        
          </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+    return(
+        <div className = "container section project-details">
+            <div className = "card z-depth-0">
+                <div className = "cord-content">
+                    <span className = "card-title">Project Title</span>
+                    <p>Lorem ipsum</p>
+                </div>
+                <div className ="card-action gret lighten-4 grey-text">
+                    <div>Posted by me</div>
+                    <div> 2nd</div>
+                </div>
+            </div>        
+        </div>
+    )
+}
