08c9c9d1f828cf3febefe55be4277cb3cdc4fdff
ricardobah <rickribeiro99@gmail.com>
2019-03-26 12:31:19 -0300

    dashbord, summary and new list

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -8,10 +8,10 @@ render(){
             <div className="row">
                 <div className="col s12 m6">
                 <ProjectList/>
+                </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
                     </div>
-                </div>
             </div>
         </div>
 
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,44 +1,12 @@
 import React from 'react'
+import ProjectSummary from  './ProjectSummary.js'
 
 const ProjectList = () => {
     return (
-        <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ricardo Ribeiro</p>
-                    <p className="grey-text" >3rd September, 2am</p>
-                </div>            
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ricardo Ribeiro</p>
-                    <p className="grey-text" >3rd September, 2am</p>
-                </div>            
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ricardo Ribeiro</p>
-                    <p className="grey-text" >3rd September, 2am</p>
-                </div>            
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ricardo Ribeiro</p>
-                    <p className="grey-text" >3rd September, 2am</p>
-                </div>            
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ricardo Ribeiro</p>
-                    <p className="grey-text" >3rd September, 2am</p>
-                </div>            
-            </div>
-        </div>    
+       <div className="project-list section">
+        <ProjectSummary />
+        <ProjectSummary />
+       </div>
         
     )
 
