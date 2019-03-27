8897d3d71c8d951ec0d8840d6af25ce4c8268004
ricardobah <rickribeiro99@gmail.com>
2019-03-27 16:42:16 -0300

    create obj

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,9 +1,18 @@
 export const createProject = (project) => {
     return(dispatch,getState, {getFirebase, getFirestore}) =>{
 
-
-
-        dispatch({type:'CREATE_PROJECT',project});
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'met',
+            authorLastName: 'nija',
+            authorID: 1235,
+            createdAt: new Date()
+        }).then(()=>{
+            dispatch({type:'CREATE_PROJECT',project});
+        }).catch((err) =>{
+            dispatch({type:'CREATE_PROJECT_ERROR',err});
+        })
 
     }
 
